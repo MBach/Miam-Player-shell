0bb08ad52baf66f7f5663ca338dfcb997189c864
Matthieu Bachelier <bachelier.matthieu@gmail.com>
2015-06-26 00:57:03 +0200

    API updated

--- MiamShellGui/mediaplaylist.h
+++ MiamShellGui/mediaplaylist.h
@@ -5,19 +5,29 @@
 
 #include "miamcore_global.h"
 
+/**
+ * \brief		The MediaPlaylist class is has been created to have a custom Random mode.
+ * \details		Default Random mode doesn't keep in memory which tracks that were played. It can be very confusing to press 'Next'
+ *				and to listen the track that just has been played before. Now, it's impossible to have the same track beein played twice
+ *				unless all other tracks were played once. Moreover if one skips a track, it's still possible to rewind and play the latter.
+ * \author      Matthieu Bachelier
+ * \copyright   GNU General Public License v3
+ */
 class MIAMCORE_LIBRARY MediaPlaylist : public QMediaPlaylist
 {
 	Q_OBJECT
 private:
 	std::vector<int> _randomIndexes;
+	int _idx;
 
 public:
 	explicit MediaPlaylist(QObject *parent = NULL);
 
-	//int currentIndex() const;
+	void shuffle(int idx);
 
-public slots:
-	void next();
+	void skipBackward();
+
+	void skipForward();
 
 private:
 	void createRandom();
